e8bd7f53b662b0f8564336b4eed4d3946b2a4606
rekkit <vuksa.jovic@gmail.com>
2018-05-26 10:51:04 +0200

    Wikipedia data
    Adding the get_wikipedia_data function to the utilities.py file.

--- 05_unsupervised_deep_learning/utilities.py
+++ 05_unsupervised_deep_learning/utilities.py
@@ -2,6 +2,7 @@
 # https://udemy.com/deep-learning-recurrent-neural-networks-in-python
 import numpy as np
 import string
+import os
 
 def all_parity_pairs(nbit, dtype=np.float32):
     # total number of samples (Ntotal) will be a multiple of 100
@@ -61,3 +62,105 @@ def get_robert_frost():
                 sentence.append(idx)
             sentences.append(sentence)
     return sentences, word2idx
+
+
+def get_wikipedia_data(n_files=None, vocab_size_limit=None, mandatory_words=[]):
+    # get the names of all the files we want to read
+    # DEVNOTE: first iteration - read only the files from folder AA
+    prefix = "large_files/AA/"
+    input_files = [file for file in os.listdir(prefix) if file.startswith("wiki")]
+    sample_vocabulary = (vocab_size_limit is not None)
+
+    # get only the specified number of files (if a number is specified)
+    if n_files is not None:
+        input_files = input_files[:n_files]
+
+    # create the list to hold the sentences
+    sentences = []
+
+    # create the word2idx and idx2word mappings and the variable that will hold the index of the next word to add
+    word2idx = {"START": 0, "END": 1}
+    # idx2word = {0: "START", 1: "END"}
+    current_idx = 2
+
+    if sample_vocabulary:
+        word_count = {"START": np.inf, "END": np.inf}
+
+    # create a flag that tells us whether or not the previous line was HTML, meaning the next is a header and should
+    # not be added to the list of sentences
+    skip_next_line = False
+
+    for file_name in input_files:
+        for line in open('large_files/AA/' + file_name, encoding="utf8"):
+            # skip HTML tag and set the flag that skips the next line, which is a header
+            if line[0] == "<":
+                skip_next_line = True
+
+            # skip the end of the HTML tag
+            elif line[:2] == "</":
+                pass
+
+            # skip header
+            elif skip_next_line:
+                skip_next_line = False
+
+            # only add line if it's not empty, i.e. if it has length larger than 1
+            elif len(line) > 1:
+                # create list to hold the words in the current line
+                sentence = []
+
+                # strip of punctuation, convert to lowercase and split the words
+                tokens = remove_punctuation(line.lower()).split()
+
+                # now go through the words and add their index to the sentence
+                for word in tokens:
+                    if word in word2idx:
+                        sentence.append(word2idx[word])
+                        if sample_vocabulary:
+                            word_count[word] += 1
+                    else:
+                        word2idx[word] = current_idx
+                        sentence.append(current_idx)
+                        if sample_vocabulary:
+                            word_count[word] = 1
+                        current_idx += 1
+
+                sentences.append(sentence)
+
+    # return the full result if a vocabulary sample size was not specified
+    if not sample_vocabulary:
+        return sentences, word2idx
+
+    # create idx to word dictionary
+    idx2word = {v: k for k, v in word2idx.items()}
+
+    # if there was a limit specified, do extra processing
+    sorted_word_count = sorted(word_count.items(), key=lambda x: x[1], reverse=True)
+
+    # create the truncated word2idx dictionary
+    word2idx_truncated = {t[0]: idx for idx, t in enumerate(sorted_word_count[:vocab_size_limit])}
+
+    # add the mandatory words, i.e. the words we don't want to fall under OTHER, to the word2idx dictionary
+    idx = max(word2idx_truncated.values()) + 1
+    for word in mandatory_words:
+        if word not in word2idx_truncated.keys():
+            word2idx_truncated[word] = idx
+            idx += 1
+
+    # finally, put all the other words under OTHER
+    word2idx_truncated["OTHER"] = idx
+    sentences_truncated = []
+
+    # create the truncated version of the sentence list
+    for sentence in sentences:
+        sentence_truncated = []
+        for word_idx in sentence:
+            if idx2word[word_idx] in word2idx_truncated:
+                sentence_truncated.append(word2idx_truncated[idx2word[word_idx]])
+            else:
+                sentence_truncated.append(word2idx_truncated["OTHER"])
+
+        sentences_truncated.append(sentence_truncated)
+
+    # return truncated values
+    return sentences_truncated, word2idx_truncated
